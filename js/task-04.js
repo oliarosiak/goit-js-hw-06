189fe7e835375ca13faebeea1564e6b7dbf675ca
Olia Rosiak <olia.rosiak@gmail.com>
2022-01-13 02:06:03 +0200

    task-04

---
+++ js/task-04.js
@@ -0,0 +1,28 @@
+/**Счетчик состоит из спана и кнопок, которые, при клике, 
+ * должны увеличивать и уменьшать его значение на единицу.
+ * 
+ * 1. Создай переменную counterValue в которой будет храниться 
+ * текущее значение счетчика и инициализируй её значением 0.
+ * 
+ * 2. Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай 
+ * значение счтетчика.
+ * 
+ * 3.Обновляй интерфейс новым значением переменной counterValue
+*/
+
+const btnDecrement = document.querySelector('button[data-action="decrement"]');
+const btnIncrement = document.querySelector('button[data-action="increment"]');
+
+let counterValue = 0;
+
+btnDecrement.addEventListener('click', () => { 
+    counterValue -= 1;
+    document.getElementById('value').textContent = counterValue;
+    console.log('Віднімає -1:', counterValue);      
+});
+
+btnIncrement.addEventListener("click", () => {
+    counterValue += 1;
+    document.getElementById('value').textContent = counterValue;
+    console.log('Додає +1:', counterValue);        
+});
